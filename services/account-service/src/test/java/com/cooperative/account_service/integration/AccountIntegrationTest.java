7eb490ff7a200d8895f046391d3b5f7b976af7fd
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-08-17 10:20:56 -0500

    fix: update TestRestTemplate import for Spring Boot 4.1.0

--- services/account-service/src/test/java/com/cooperative/account_service/integration/AccountIntegrationTest.java
+++ services/account-service/src/test/java/com/cooperative/account_service/integration/AccountIntegrationTest.java
@@ -3,7 +3,7 @@ package com.cooperative.account_service.integration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
