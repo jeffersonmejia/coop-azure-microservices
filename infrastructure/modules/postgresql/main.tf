4df25fcd566d7702d0da19aa8f0dad6d9071f319
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-08-29 19:57:01 -0500

    fix: use write-only PostgreSQL password

--- infrastructure/modules/postgresql/main.tf
+++ infrastructure/modules/postgresql/main.tf
@@ -34,8 +34,9 @@ resource "azurerm_postgresql_flexible_server" "this" {
   resource_group_name           = var.resource_group_name
   location                      = var.location
   version                       = "17"
-  administrator_login           = var.administrator_login
-  administrator_password        = var.administrator_password == null ? null : var.administrator_password
+  administrator_login               = var.administrator_login
+  administrator_password_wo         = var.administrator_password
+  administrator_password_wo_version = 1
   sku_name                      = "B_Standard_B1ms"
   storage_mb                    = 32768
   storage_tier                  = "P4"
@@ -53,7 +54,6 @@ resource "azurerm_postgresql_flexible_server" "this" {
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes  = [administrator_password]
   }
 }
 
